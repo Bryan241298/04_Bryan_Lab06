56e22d5d006dd81533128970147e576dba2935b9
Bryan241298 <bryan_goh_@hotmail.com>
2023-01-18 19:05:36 +0800

    Added Lab06

---
+++ 04_Bryan_Lab6/Assets/CalcScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcScript : MonoBehaviour
+{
+    public Toggle USdollars;
+    public Toggle JapaneseYen;
+
+    public Text value;
+
+    private string input;
+    public double inputv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value.text = "$ " + inputv;
+    }
+
+    public void ReadStringInput(string s)
+    {
+        input = s;
+
+        double.TryParse(input, out inputv);
+    }
+
+    public void convertBtn()
+    {
+        if (USdollars.isOn)
+        {
+            inputv *= 0.74f;
+        }
+
+        if (JapaneseYen.isOn)
+        {
+            inputv *= 82.78f;
+        }
+
+        value.text = "$ " + inputv;
+    }
+
+    public void clearBtn(InputField s)
+    {
+        s.text = "" + 0;
+        value.text = "" + s;
+
+        USdollars.isOn = false;
+        JapaneseYen.isOn = false;
+    }
+}
